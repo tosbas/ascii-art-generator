c6d9acac2e9f9d3cd0140f644c1341cacbecfed5
tosbas <tosbasdev@protonmail.com>
2025-04-01 17:20:06 +0200

    Ajout des contrôles

--- main.js
+++ main.js
@@ -1,38 +1,69 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+
+const downloader = document.getElementById("downloader");
+const fileInput = document.getElementById("file-input");
+
+const scaleSlider = document.getElementById("scaleSlider");
+const multSlider = document.getElementById("multSlider");
+const fontSizeSlider =document.getElementById("fontSizeSlider");
 
 const img = new Image();
 img.src = "ut-2004.png";
 
 const charList = "@#%*+=-:. ";
 
-// Multiplier for spacing of characters
-const multX = 6;  // Defines the horizontal spacing multiplier between ASCII characters.
-const multY = 8;  // Defines the vertical spacing multiplier between ASCII characters.
+let mult = multSlider.value;
 
-// Scale
-const scale = 0.4;  // Defines the scaling factor for the image.
+let scale = scaleSlider.value;
 
-// Font Size
-const fontSize = 10;  // Defines the font size used to display the ASCII characters on the canvas.
+let fontSize = 10;
 
+fileInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+        img.src = e.target.result;
+    };
+});
 
 img.onload = function () {
- 
+    process = true;
+    renderImage();
+};
+
+function handleScale(e) {
+    scale = parseFloat(e.target.value);
+    renderImage();
+}
+
+function handleMult(e) {
+    mult = parseFloat(e.target.value); 
+    renderImage();
+}
+
+function handleFontSize(e){
+    fontSize = parseFloat(e.target.value); 
+    renderImage();
+}
+
+function renderImage() {
     const asciiW = Math.floor(img.width * scale);
     const asciiH = Math.floor(img.height * scale);
 
-    canvas.width = asciiW * multX;
-    canvas.height = asciiH * multY; 
-
-    ctx.drawImage(img, 0, 0, asciiW, asciiH);
-    const imageData = ctx.getImageData(0, 0, asciiW, asciiH).data;
+    canvas.width = asciiW * mult;
+    canvas.height = asciiH * mult;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-   
     ctx.font = `${fontSize}px monospace`;
+
+
+    ctx.drawImage(img, 0, 0, asciiW, asciiH);
+    const imageData = ctx.getImageData(0, 0, asciiW, asciiH).data;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let y = 0; y < asciiH; y++) {
         for (let x = 0; x < asciiW; x++) {
@@ -41,17 +72,33 @@ img.onload = function () {
             let g = imageData[index + 1];
             let b = imageData[index + 2];
 
-            // YIQ formula for better contrast https://en.wikipedia.org/wiki/YIQ
             let brightness = r * 0.299 + g * 0.587 + b * 0.114;
             let charIndex = Math.floor((brightness / 255) * (charList.length - 1));
-
             let char = charList[charList.length - 1 - charIndex];
+
+
             ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-            ctx.fillText(char, x * multX, y * multY);
+            ctx.fillText(char, x * mult, y * mult);
         }
- 
     }
-};
+
+}
+
+fontSizeSlider.addEventListener("change", (e)=> handleFontSize(e));
+scaleSlider.addEventListener("change", (e) => handleScale(e));
+multSlider.addEventListener("change", (e) => handleMult(e));
+downloader.addEventListener("click", (e) => download(e));
+
+function download(e) {
+    if (e.isTrusted) {
+        e.preventDefault();
+    }
+
+    downloader.href = canvas.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+    e.target.click();
+}
+
+
 
 
 
